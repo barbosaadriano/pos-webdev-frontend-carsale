7883d393272f4ca2e0be53c0c68bd314d8385eb2
root <root@jarvis.avantagem>
2016-08-27 10:25:05 -0300

    commit inicial aula front end

---
+++ base.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Car Sale - Especialista em carros antigos e Relíquias </title>
+			<meta charset="utf-8">
+			<meta name="description" content="Revenda de carros, novos e usuados de alta qualidade">
+			<meta name="keywords" content="carros,novos,usados,importados,ivate">
+			<meta name="viewport" content="width=device-width,initial-scale=1">
+			
+			<!--meta tag do facebook -->
+			<meta property="og:locale" content="pt_BR">
+			<meta property="og:title" content="Car Sale - Adriano Barbosa">
+			<meta property="og:description" content="revenda de carros novos e usados de alta qualidade">
+			<meta property="og:image" content="http://pos.professorburnes.com.br/carsale/img/carsale.jpg">
+			<meta property="og:image:type" content="image/jpeg">
+			<meta property="og:image:width" content="800">
+			<meta property="og:image:height" content="315">
+			<meta property="og:type" content="website">
+
+			<!--twitter card -->
+
+			<meta property="twitter:card" content="summary">			
+			<meta property="twitter:url" content="@professorburnes">			
+			<meta property="twitter:image" content="http://pos.professorburnes.com.br/carsale/img/carsale.jpg">
+			<meta property="twitter:title" content="Revenda de carros, novos e usuados de alta qualidade">
+			
+			<!-- css -->
+			<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+			<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<link rel="icon" type="image/png" href="imgs/icone.png">
+			
+			<!-- JavasCript -->
+			<script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+			<script type="text/javascript" src="js/bootstrap.min.js"></script>
+			<script type="text/javascript" src="js/bootstrap-inputmask.min.js"></script>
+			<script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+			
+				
+		</head>
+		<body>
+
+		</body>
+	</html>
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Car Sale - Especialista em carros antigos e Relíquias </title>
+			<meta charset="utf-8">
+			<meta name="description" content="Revenda de carros, novos e usuados de alta qualidade">
+			<meta name="keywords" content="carros,novos,usados,importados,ivate">
+			<meta name="viewport" content="width=device-width,initial-scale=1">
+			
+			<!--meta tag do facebook -->
+			<meta property="og:locale" content="pt_BR">
+			<meta property="og:title" content="Car Sale - Adriano Barbosa">
+			<meta property="og:description" content="revenda de carros novos e usados de alta qualidade">
+			<meta property="og:image" content="http://pos.professorburnes.com.br/carsale/img/carsale.jpg">
+			<meta property="og:image:type" content="image/jpeg">
+			<meta property="og:image:width" content="800">
+			<meta property="og:image:height" content="315">
+			<meta property="og:type" content="website">
+
+			<!--twitter card -->
+
+			<meta property="twitter:card" content="summary">			
+			<meta property="twitter:url" content="@professorburnes">			
+			<meta property="twitter:image" content="http://pos.professorburnes.com.br/carsale/img/carsale.jpg">
+			<meta property="twitter:title" content="Revenda de carros, novos e usuados de alta qualidade">
+			
+			<!-- css -->
+			<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+			<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+			<link rel="stylesheet" type="text/css" href="css/style.css">
+			<link rel="icon" type="image/png" href="imgs/icone.png">
+			
+			<!-- JavasCript -->
+			<script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+			<script type="text/javascript" src="js/bootstrap.min.js"></script>
+			<script type="text/javascript" src="js/bootstrap-inputmask.min.js"></script>
+			<script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+			
+				
+		</head>
+		<body>
+			<header>
+				<div class="container">
+					<a href="index.php" title="Home" class="logo">
+						<img src="imgs/logo.png" alt="CarSale">
+						Car Sale
+					</a>
+					<div class="info">
+						<span><i class="fa fa-phone"></i> 0800 011 1406</span>
+						<a href="#" title="facebook">
+							<i class="fa fa-facebook"></i>
+						</a>
+						<a href="#" title="twitter">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="#" title="instagran">
+							<i class="fa fa-instagram"></i>
+						</a>
+						
+					</div>
+				</div>
+			</header>
+		</body>
+	</html>

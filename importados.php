34b163a837bfa87bd395656b7bcd767469edd129
abarbosa <abarbosa@jarvis.avantagem>
2016-09-21 12:45:34 -0300

    Finalização das páginas

---
+++ importados.php
@@ -0,0 +1,92 @@
+<div class="container carros">
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/15.jpg" alt="carro 15" class="img-responsive">
+                    <h3>Land Rover</h3>
+                    <a href="#Carro15" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/16.jpg" alt="carro 16" class="img-responsive">
+                    <h3>Rover Land</h3>
+                    <a href="#Carro16" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/17.jpg" alt="carro 17" class="img-responsive">
+                    <h3>Range Rover</h3>
+                    <a href="#Carro17" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/18.jpg" alt="carro 18" class="img-responsive">
+                    <h3>Outro Range Rover</h3>
+                    <a href="#Carro18" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/19.jpg" alt="carro 19" class="img-responsive">
+                    <h3>Range Rover outra vez</h3>
+                    <a href="#Carro19" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/20.jpg" alt="carro 20" class="img-responsive">
+                    <h3>Range Rover 2</h3>
+                    <a href="#Carro20" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/ferrari.jpg" alt="carro 21" class="img-responsive">
+                    <h3>Ferrari vermelha</h3>
+                    <a href="#Carro21" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/corvete.jpg" alt="carro 22" class="img-responsive">
+                    <h3>Corvete vermelho</h3>
+                    <a href="#Carro22" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ nacionais.php
@@ -0,0 +1,115 @@
+<div class="container carros">
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/4.jpg" alt="carro 4" class="img-responsive">
+                    <h3>Fiat Punto</h3>
+                    <a href="#Carro4" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/5.jpg" alt="carro 5" class="img-responsive">
+                    <h3>Fiat IDEA</h3>
+                    <a href="#Carro5" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/6.jpg" alt="carro 6" class="img-responsive">
+                    <h3>Gran Siena</h3>
+                    <a href="#Carro6" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/7.jpg" alt="carro 7" class="img-responsive">
+                    <h3>Fiat Doblo</h3>
+                    <a href="#Carro7" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/8.jpg" alt="carro 8" class="img-responsive">
+                    <h3>Fiat ideiaFIX</h3>
+                    <a href="#Carro8" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/9.jpg" alt="carro 9" class="img-responsive">
+                    <h3>Fiat não sei o nome</h3>
+                    <a href="#Carro9" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/10.jpg" alt="carro 10" class="img-responsive">
+                    <h3>Doblo denovo</h3>
+                    <a href="#Carro10" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/11.jpg" alt="carro 11" class="img-responsive">
+                    <h3>Fiat 500</h3>
+                    <a href="#Carro11" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/12.jpg" alt="carro 12" class="img-responsive">
+                    <h3>Palio</h3>
+                    <a href="#Carro12" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="thumbnail">
+                <div class="wrap">
+                    <img src="imgs/13.jpg" alt="carro 13" class="img-responsive">
+                    <h3>Freemont</h3>
+                    <a href="#Carro13" class="btn btn-danger">
+                        <i class="fa fa-search"></i> Detalhes
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
